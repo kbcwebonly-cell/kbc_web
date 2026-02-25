514f64bb2a95d86db3d356c49e81fc634edc4bde
kbcwebonly-cell <kbcwebonly@gmail.com>
2026-02-25 13:22:12 +0800

    Add files via upload

---
+++ version/animate.js
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Jun 2000 09:06:00 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_cacheddtm:TX|05 Jun 2000 09:06:00 -0000
+vti_filesize:IR|14261
+vti_backlinkinfo:VX|index4.htm
